cdf06e69c60bfb4a50e83c37aae953b8e6ba6772
pedro-psb <pedropsb95@gmail.com>
2023-02-23 20:27:31 -0300

    add gh-pages to deploy

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,4 @@
-path = require(`path`)
+path = require(`path`);
 
 module.exports = {
   siteMetadata: {
@@ -9,7 +9,8 @@ module.exports = {
     // Used for SEO
     description: `Pedro Pessoa portifolio`,
     // Used for resolving images in social cards
-    siteUrl: `https://example.com`,
+    siteUrl: `https://pedro-psb.github.io/gatsby-portifolio-website/`,
+    pathPrefix: '/gatsby-portifolio-website ',
     // Used for social links in the root footer
     social: [
       {
@@ -31,8 +32,8 @@ module.exports = {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'images',
-        path: path.join(__dirname, 'src', 'images')
-      }
+        path: path.join(__dirname, 'src', 'images'),
+      },
     },
     'gatsby-plugin-mantine',
     // 'gatsby-plugin-sharp',
